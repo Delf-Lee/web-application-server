403265fb620e89735ad37033e8c12303b70b7978
Delf-Lee <ttjs2008@naver.com>
2018-07-25 08:42:39 +0900

    HttpRequest 클래스 작성

--- src/main/java/util/HttpRequest.java
+++ src/main/java/util/HttpRequest.java
@@ -18,13 +18,48 @@ public class HttpRequest {
     Map<String, String> headers = new HashMap<>();
     Map<String, String> params = new HashMap<>();
 
-
     public HttpRequest(InputStream in) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            if (line == null) {
+                return;
+            }
+            processRequestLine(line);
 
+            line = br.readLine();
+            while (!line.equals("")) {
+                log.debug("header : {}", line);
+                String[] tokens = line.split(":");
+                headers.put(tokens[0].trim(), tokens[1].trim());
+                line = br.readLine();
+            }
+
+            if ("POST".equals(method)) {
+                String body = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+                params = HttpRequestUtils.parseQueryString(body);
+            }
+        } catch (IOException io) {
+            log.error(io.getMessage());
+        }
     }
 
     private void processRequestLine(String requestLine) {
+        log.debug("request line : {}", requestLine);
+        String[] tokens = requestLine.split(" ");
+        method = tokens[0];
+        if ("POST".equals(method)) {
+            path = tokens[1];
+            return;
+        }
 
+        int index = tokens[1].indexOf("?");
+        if (index == -1) {
+            path = tokens[1];
+        } else {
+            path = tokens[1].substring(0, index);
+            params = HttpRequestUtils.parseQueryString(tokens[1].substring(index + 1));
+        }
     }
 
     public String getMethod() {
